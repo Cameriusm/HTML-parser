992b9755a0d32b974dd0e10e8d3a5301ce6e1435
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-04-18 17:47:55 +0700

    Connect to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyDn29MJXCAKSRQyA2qwqFLjTAn-OUUBMjU',
+  authDomain: 'auth-parser.firebaseapp.com',
+  projectId: 'auth-parser',
+  storageBucket: 'auth-parser.appspot.com',
+  messagingSenderId: '29473829559',
+  appId: '1:29473829559:web:2775da040439f0d3385332',
 });
 
 export const auth = app.auth();
