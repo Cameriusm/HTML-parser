576b526357f202bc20601bb488f36aed7bef554a
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-04-22 02:26:34 +0700

    Add parsed objects to dashboard

--- src/components/ParserDetails.js
+++ src/components/ParserDetails.js
@@ -5,38 +5,42 @@ import axios from 'axios';
 
 export default function Parser(props) {
   console.log(props);
-  const [parser, setParser] = useState({});
+  const [parser, setParser] = useState([]);
 
   useEffect(() => {
     console.log(props.location.state.categories);
-    axios.get('http://localhost:3001/api/get', {
-      categories: props.location.state.categories,
-    });
-  });
+    axios
+      .post('http://localhost:3001/api/post', {
+        categories: props.location.state.categories,
+      })
+      .then((result) => {
+        const results = [].concat.apply([], result.data);
+        setParser(results);
+        console.log(result.data);
+      });
+  }, []);
 
   const handleExcel = () => {
     console.log(props);
   };
 
-  //   const parseHandler = (e) => {
-  //     e.preventDefault();
-  //     setLoading(true);
-  //     setError('');
-  //     if (boxes.length === 0) {
-  //       setError('Вы не выбрали ни одной категории!');
-  //     } else {
-  //       history.push({
-  //         pathname: '/parsing-details',
-  //         state: { categories: boxes },
-  //       });
-  //     }
-  //     setLoading(false);
-  // console.log(boxes);
-
   return (
     <div className="parsing-background checkbox-category-background">
       <div className="parsing-details checkbox-list">
         <h2>Результат работы парсера:</h2>
+        <div>
+          {parser.map((val) => {
+            return (
+              <div className="card" key={val.articul}>
+                <img src={val.img} alt="" />
+                <h1>{val.name}</h1>
+                <p>{val.description}</p>
+
+                <button>Delete</button>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div className="parsing-bottom">
         <div className="parsing-button ">
